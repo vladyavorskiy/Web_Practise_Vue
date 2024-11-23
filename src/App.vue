f1b57f663ec37418cce142c320d2836752800f27
Vladislav Yavorskij <yavorskij_vi@edu.surgu.ru>
2024-11-24 04:07:39 +0500

    PrimeVue

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,77 @@
 <template>
-  <header>
-    <nav>
-      <ul>
-        <li><router-link to="/">Главная</router-link></li>
-        <li><router-link to="/countries">Страны</router-link></li>
-        <li><router-link to="/events">События</router-link></li>
-      </ul>
-      <div v-if="isAuthenticated && user">
-        Welcome, {{ user.name }}
-        <button @click="logout">Logout</button>
+  <PrimeMenubar :model="items">
+    <template #start>
+      <span>
+        <img src="@/assets/planet-earth.png" width="50" alt="My PNG Icon"/>
+      </span>
+    </template>
+    <template #item="{ item }">
+      <a class="flex items-center ml-6 p-4">
+        <router-link v-if="item.route" :to="item.route">
+          <span :class="item.icon"/>
+          <span class="ml-1">{{ item.label }}</span>
+        </router-link>
+      </a>
+    </template>
+    <template #end>
+      <div class="flex items-center gap-2">
+        <div v-if="isAuthenticated && user">
+          <span class="pi pi-fw pi-user mr-4"/> {{ user.name }}
+          <PrimeButton @click="logout" class="ml-4">Выйти</PrimeButton>
+        </div>
+        <div v-else>
+          <form @submit.prevent="login">
+            <PrimeInputText v-model="email" type="email" id="email" required placeholder="Логин" class="m-2 sm:w-auto"
+                       :class="{'p-invalid': authError}"/>
+            <PrimeInputText v-model="password" type="password" id="password" required placeholder="Пароль"
+                       class="m-2 sm:w-auto" :class="{'p-invalid': authError}"/>
+            <PrimeButton type="submit">Войти</PrimeButton>
+            <div class="ml-2"><small v-if="authError" class="error">{{ authError }}</small></div>
+          </form>
+        </div>
       </div>
-      <div v-else>
-        <form @submit.prevent="login">
-          <div>
-            <label for="email">Email:</label>
-            <input v-model="email" type="email" id="email" required />
-          </div>
-          <div>
-            <label for="password">Password:</label>
-            <input v-model="password" type="password" id="password" required />
-          </div>
-          <button type="submit">Login</button>
-          <p v-if="authError" class="error">{{ authError }}</p>
-        </form>
-      </div>
-    </nav>
-  </header>
+    </template>
+  </PrimeMenubar>
   <router-view></router-view>
 </template>
 
-
 <script>
 import { useAuthStore } from '@/stores/authStore';
+import Button from 'primevue/button';
+import Menubar from 'primevue/menubar';
+import InputText from 'primevue/inputtext'
 export default {
+  components: {
+    PrimeButton: Button,
+    PrimeMenubar: Menubar,
+    PrimeInputText: InputText,
+  },
   data() {
     return {
       email: '',
       password: '',
       authStore: useAuthStore(),
+      items: [
+        {
+          label: 'Главная страница',
+          icon: 'pi pi-fw pi-home',
+          route: '/',
+          shortcut: 'Ctrl + H',
+          submenu: [
+            // Submenu items
+          ],
+        },
+        {
+          label: 'Страны',
+          icon: 'pi pi-fw pi-map-marker',
+          route: '/countries',
+        },
+        {
+          label: 'События',
+          icon: 'pi pi-fw pi-calendar',
+          route: '/events',
+        },
+      ]
     };
   },
   computed: {
--- src/main.js
+++ src/main.js
@@ -2,10 +2,23 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+import 'primeicons/primeicons.css';
 
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue,{
+  theme:{
+    preset: Aura,
+    options:{
+      prefix: 'p',
+      darkModeSelector: 'system',
+      cssLayer: false
+    }
+  }
+})
 app.mount('#app')
 
 
